1cc881e8b41aaa117e802645e001aeeeb638f762
samuelireson <107627922+samuelireson@users.noreply.github.com>
2023-07-11 13:49:11 +0000

    Tasks 2,9

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,7 +7,9 @@ const MovieList = (props) => {
 		<>
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt={movie}></img>
+					<div className='sidepadding'>
+						<img src={movie.Poster} alt={movie.Title}></img>
+					</div>
 					<div
 						onClick={() => props.handleFavouritesClick(movie)}
 						className='overlay d-flex align-items-center justify-content-center'
